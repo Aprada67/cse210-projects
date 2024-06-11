53e62e0398b5b1ea12783e0c6dc2207c071e9b28
Aprada67 <albertojprada10@gmail.com>
2024-06-11 11:53:57 +0100

    Finished Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,60 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string valueFromUser = Console.ReadLine();
+
+        int grade = int.Parse(valueFromUser);
+        
+        // Determinate the letter
+
+        string letter = "";
+
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        else
+        {
+            letter = "F";
+        }
+
+        // Determinate the sign
+
+        string sign = "";
+        int lastDigit = grade % 10;
+
+        if (letter != "F")
+        {
+            if (lastDigit >= 7)
+            {
+                sign = "+";
+            }
+            else if (lastDigit < 3)
+            {
+                sign = "-";
+            }
+        }
+
+        // Manage letter A
+
+        if (letter == "A" && sign == "+")
+        {
+            sign = "";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}{sign}");
     }
 }
