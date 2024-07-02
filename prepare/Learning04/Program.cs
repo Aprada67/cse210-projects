0479fec094c90df798fb5acee28b33b0411d9fa9
Aprada67 <albertojprada10@gmail.com>
2024-07-02 10:44:44 +0100

    Finished Prep04

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment a1 = new Assignment("Alberto Prada", "Chemical");
+        Console.WriteLine(a1.GetSummary());
+        Console.WriteLine();
+
+        MathAssignment a2 = new MathAssignment("Alberto Prada", "Math", "Multiplication", "8 x 9 = ?");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+        Console.WriteLine();
+
+        WritingAssignment a3 = new WritingAssignment("Alberto Prada", "Venezuelan History", "The Battle of Carabobo");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWrittingInformation());
+        Console.WriteLine();
     }
 }
