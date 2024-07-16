ff8b5596310468f6d0d30340ee5afe5b3ad63b03
Aprada67 <albertojprada10@gmail.com>
2024-07-16 20:12:43 +0100

    Finished Program 1

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -4,6 +4,51 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation1 World!");
+        var comment1 = new List<Comments>
+        {
+            new Comments("Robert", "Like if you are listening this song in 2024"),
+            new Comments("Cristina", "Amazing 😍"),
+            new Comments("Jeff", "It is music to my ears!!"),
+        };
+
+        Video video1 = new Video("Dex Arson & Incandescent - Of Light And Fire", "Dex Arson", 307, comment1);
+
+        var comment2 = new List<Comments>
+        {
+            new Comments("NoCopyrigthSounds", "Do you feel your bones start to shake? Do you feel the earthquake?  0:34 🔥"),
+            new Comments("Jonathan", "ROY KNOX continues to make good stuff! Was not expecting that 3rd drop to be the heaviest!"),
+            new Comments("JesusPerez", "Soon enough, we'll hear this in gaming montages."),
+        };
+
+        Video video2 = new Video("ROY KNOX - Earthquake | Drumstep | NCS - Copyright Free Music", "NoCopyrigthSounds", 178, comment2);
+
+        var comment3 = new List<Comments>
+        {
+            new Comments("SolenceOfficial", "Thanks for all these amazing comments!! Take care everyone and wash your hands!! <3"),
+            new Comments("Emryth", "Solence is way too underrated."),
+            new Comments("FrostPlayer", "After two years, I still love listening to it"),
+        };
+
+        Video video3 = new Video("Solence - Blackout (Official Lyric Video)", "SolenceOfficial", 200, comment3);
+
+        var videos = new List<Video> { video1, video2, video3};
+
+        int i = 1;
+
+        foreach(Video video in videos)
+        {
+            Console.WriteLine($"Video {i}");
+            Console.WriteLine($"Title: {video.GetTitle()}");
+            Console.WriteLine($"Author: {video.GetAutor()}");
+            Console.WriteLine($"Length: {video.GetLength()} seconds");
+            Console.WriteLine($"Comments:");
+
+            foreach(Comments comments in video.GetComments())
+            {
+                Console.WriteLine($"    {comments.CreateComment()}\n");
+            }
+
+            i++;
+        }
     }
 }
---
+++ final/Foundation1/Video.cs
@@ -0,0 +1,54 @@
+public class Video
+{
+    private string _title;
+    private string _author;
+    private int _length;
+    private List<Comments> _comments;
+
+    public Video(string title, string author, int length, List<Comments> comments)
+    {
+        _title = title;
+        _author = author;
+        _length = length;
+        _comments = comments;
+    }
+
+    public Video() { }
+
+    public void SetTitle(string title)
+    {
+        _title = title;
+    }
+
+    public string GetTitle()
+    {
+        return _title;
+    }
+    public void SetAuthor(string author)
+    {
+        _author = author;
+    }
+
+    public string GetAutor()
+    {
+        return _author;
+    }
+    public void SetLength(int length)
+    {
+        _length = length;
+    }
+
+    public int GetLength()
+    {
+        return _length;
+    }
+    public void SetComments(List<Comments> comments)
+    {
+        _comments = comments;
+    }
+
+    public List<Comments> GetComments()
+    {
+        return _comments;
+    }
+}
